51325d1ea4306ee98eca9306cbbd9a87d9bdd833
Vitória A. Cavalheiro <vitoriocavalheiro03@gmail.com>
2024-08-14 13:43:56 -0300

    🔧 ci: separate GitHub Actions workflows into dev and prod

--- tests/test_ipsum_generator.py
+++ tests/test_ipsum_generator.py
@@ -5,51 +5,92 @@ from pylorem.generator.lorem_ipsum import LoremIpsum
 
 class TestLoremIpsum(unittest.TestCase):
 
-    # Generates a specified number of lorem ipsum paragraphs with start_with_lorem_ipsum=True, and checks if the result has the correct number of paragraphs, starts with "Lorem ipsum", and ends with "\n \n".
-    def test_generate_paragraphs_with_start_true_fixed(self):
-        """
-        Generate paragraphs with `start_with_lorem_ipsum` set to `True`.
+    # Generates paragraphs of the specified size with fixed assertion
+    def test_generates_paragraphs_of_specified_size_with_fixed_assertion(self):
+        # Arrange
+        lorem_ipsum = LoremIpsum()
+        paragraphs_numbers = 2
+        size = "small"
+        start_with_lorem_ipsum = False
 
-        :param self: The object instance.
+        # Act
+        result = lorem_ipsum.paragraphs(paragraphs_numbers, size, start_with_lorem_ipsum)
+        generated_paragraphs = result.split("\n\n\n")
+
+        # Assert
+        for paragraph in generated_paragraphs:
+            self.assertEqual(len(paragraph.split("\n")), 3 * paragraphs_numbers)
+
+        # Handles invalid size parameter gracefully
+
+    def test_handles_invalid_size_parameter_gracefully(self):
+        # Arrange
+        lorem_ipsum = LoremIpsum()
+        paragraphs_numbers = 2
+        size = "invalid_size"
+        start_with_lorem_ipsum = False
+
+        # Act
+        result = lorem_ipsum.paragraphs(paragraphs_numbers, size, start_with_lorem_ipsum)
+        generated_paragraphs = result.split("\n\n")
+
+        # Assert
+        for paragraph in generated_paragraphs:
+            self.assertEqual(len(paragraph.split("\n")), 10)
+
+    def test_generates_zero_paragraphs_when_paragraphs_numbers_is_zero(self):
+        # Arrange
+        lorem_ipsum = LoremIpsum()
+        paragraphs_numbers = 0
+        size = "medium"
+        start_with_lorem_ipsum = False
+
+        # Act
+        result = lorem_ipsum.paragraphs(paragraphs_numbers, size, start_with_lorem_ipsum)
+
+        # Assert
+        self.assertEqual(result, "")
+
+    # Generates 0 words.
+    def test_generate_zero_words(self):
+        """
+        Generate the function comment for the given function body in a markdown code block with the correct language syntax.
+
+        :param self: The instance of the test class.
         :return: None
         """
         # Arrange
         lorem = LoremIpsum()
-        paragraphs_numbers = 3
-        start_with_lorem_ipsum = True
+        words_numbers = 0
 
         # Act
-        result = lorem.paragraphs(paragraphs_numbers, start_with_lorem_ipsum)
+        result = lorem.words(words_numbers)
 
         # Assert
-        self.assertEqual(len(result.split("\n")), paragraphs_numbers + 3)
-        self.assertTrue(result.startswith(lorem.start_with_lorem_ipsum))
-        self.assertTrue(result.endswith("\n \n"))
+        self.assertEqual(len(result.split()), words_numbers)
 
-    # Generates a specified number of lorem ipsum paragraphs with start_with_lorem_ipsum=False.
-    def test_generate_paragraphs_with_start_false_fixed(self):
+    # Generates 1 word.
+    def test_generate_one_word(self):
         """
-        Generates paragraphs of Lorem Ipsum text with the given number of paragraphs and the option to start with "Lorem Ipsum" or not.
+        Test the `words` method of the `LoremIpsum` class when generating one word.
 
-        :param paragraphs_numbers: The number of paragraphs to generate (int).
-        :param start_with_lorem_ipsum: Whether to start each paragraph with "Lorem Ipsum" or not (bool).
+        This test case checks if the `words` method of the `LoremIpsum` class correctly generates the specified number of words. The test first arranges the necessary variables by creating an instance of the `LoremIpsum` class and setting the `words_numbers` variable to 1. Then, the test acts by calling the `words` method with the `words_numbers` parameter. Finally, the test asserts that the length of the result, after splitting it into a list of words, is equal to the specified `words_numbers`.
 
-        :return: The generated Lorem Ipsum text (str).
+        This test helps ensure that the `words` method of the `LoremIpsum` class behaves as expected when generating one word.
+
+        :param self: The test case instance.
         """
+
         # Arrange
         lorem = LoremIpsum()
-        paragraphs_numbers = 5
-        start_with_lorem_ipsum = False
+        words_numbers = 1
 
         # Act
-        result = lorem.paragraphs(paragraphs_numbers, start_with_lorem_ipsum)
+        result = lorem.words(words_numbers)
 
         # Assert
-        self.assertEqual(len(result.split("\n")), paragraphs_numbers + 3)
-        self.assertFalse(result.startswith(lorem.start_with_lorem_ipsum))
-        self.assertTrue(result.endswith("\n \n"))
+        self.assertEqual(len(result.split()), words_numbers)
 
-    # Generates a string of random words.
     def test_generate_random_words(self):
         """
         Generates a random sequence of words using the LoremIpsum class.
@@ -93,125 +134,3 @@ class TestLoremIpsum(unittest.TestCase):
         # Assert
         self.assertEqual(len(result.split("\n")), items_count + 1)
         self.assertTrue(result.startswith("Shopping List:"))
-
-    # Generates 0 paragraphs.
-    def test_generate_zero_paragraphs_fixed(self):
-        """
-        Generate a zero paragraphs of lorem ipsum text.
-
-        This function takes no parameters.
-
-        Returns:
-            str: The generated lorem ipsum text.
-        """
-        # Arrange
-        lorem = LoremIpsum()
-        paragraphs_numbers = 0
-
-        # Act
-        result = lorem.paragraphs(paragraphs_numbers)
-
-        # Assert
-        self.assertEqual(len(result.split("\n")), 4)
-        self.assertTrue(result.startswith(lorem.start_with_lorem_ipsum))
-        self.assertTrue(result.endswith("\n \n"))
-
-    # Generates 1 paragraph with start_with_lorem_ipsum=True.
-    def test_generate_one_paragraph_with_start_true(self):
-        """
-        Generates a single paragraph of lorem ipsum text using the given parameters.
-
-        Args:
-            self (object): The current instance of the LoremIpsum class.
-            paragraphs_numbers (int): The number of paragraphs to generate.
-            start_with_lorem_ipsum (bool): Specifies whether the generated text should start with "Lorem ipsum dolor sit amet".
-
-        Returns:
-            str: The generated paragraph of lorem ipsum text.
-
-        Raises:
-            AssertionError: If the length of the generated text does not match the expected value.
-            AssertionError: If the generated text does not start with the specified prefix.
-            AssertionError: If the generated text does not end with the expected suffix.
-        """
-        # Arrange
-        lorem = LoremIpsum()
-        paragraphs_numbers = 1
-        start_with_lorem_ipsum = True
-
-        # Act
-        result = lorem.paragraphs(paragraphs_numbers, start_with_lorem_ipsum)
-
-        # Assert
-        self.assertEqual(len(result.split("\n")), 4)
-        self.assertTrue(result.startswith(lorem.start_with_lorem_ipsum))
-        self.assertTrue(result.endswith("\n \n"))
-
-    # Generates 1 paragraph with start_with_lorem_ipsum=False.
-    def test_generate_one_paragraph_with_start_false_fixed(self):
-        """
-        Generates a single paragraph of Lorem Ipsum text using the given parameters.
-
-        :param paragraphs_numbers: The number of paragraphs to generate. Only 1 paragraph is generated in this case.
-        :type paragraphs_numbers: int
-
-        :param start_with_lorem_ipsum: Whether the generated paragraph should start with "Lorem ipsum" or not. In this case, it is set to False.
-        :type start_with_lorem_ipsum: bool
-
-        :return: The generated Lorem Ipsum paragraph.
-        :rtype: str
-        """
-
-        # Arrange
-        lorem = LoremIpsum()
-
-        paragraphs_numbers = 1
-        start_with_lorem_ipsum = False
-
-        # Act
-        result = lorem.paragraphs(paragraphs_numbers, start_with_lorem_ipsum)
-
-        # Assert
-        self.assertEqual(len(result.split("\n")), 4)
-        self.assertFalse(result.startswith(lorem.start_with_lorem_ipsum))
-        self.assertTrue(result.endswith("\n \n"))
-
-    # Generates 0 words.
-    def test_generate_zero_words(self):
-        """
-        Generate the function comment for the given function body in a markdown code block with the correct language syntax.
-
-        :param self: The instance of the test class.
-        :return: None
-        """
-        # Arrange
-        lorem = LoremIpsum()
-        words_numbers = 0
-
-        # Act
-        result = lorem.words(words_numbers)
-
-        # Assert
-        self.assertEqual(len(result.split()), words_numbers)
-
-    # Generates 1 word.
-    def test_generate_one_word(self):
-        """
-        Test the `words` method of the `LoremIpsum` class when generating one word.
-
-        This test case checks if the `words` method of the `LoremIpsum` class correctly generates the specified number of words. The test first arranges the necessary variables by creating an instance of the `LoremIpsum` class and setting the `words_numbers` variable to 1. Then, the test acts by calling the `words` method with the `words_numbers` parameter. Finally, the test asserts that the length of the result, after splitting it into a list of words, is equal to the specified `words_numbers`.
-
-        This test helps ensure that the `words` method of the `LoremIpsum` class behaves as expected when generating one word.
-
-        :param self: The test case instance.
-        """
-
-        # Arrange
-        lorem = LoremIpsum()
-        words_numbers = 1
-
-        # Act
-        result = lorem.words(words_numbers)
-
-        # Assert
-        self.assertEqual(len(result.split()), words_numbers)
